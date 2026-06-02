de57149b4eebf7908faa2219b5a3c0b4f82ed996
abottleofvodka123-alt <abottleofvodka123@gmail.com>
2026-06-02 12:01:44 +0530

    push exe and key

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 @echo off
-set GROQ_API_KEY=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
+set GROQ_API_KEY=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
 cd /d "%~dp0"
 start "" "dist\code_solver\code_solver.exe"
